7fe2966ad5fd02bd8aa60bc4ab7b28c292d746b2
sammy wlodawski <samwlodawski@gmail.com>
2024-08-29 16:54:31 -0400

    Created server.js file for port use.

---
+++ server.js
@@ -0,0 +1,18 @@
+const expresss = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = require('express');
+
+app.use(expresss.urlencoded({extended: true}));
+app.use(expresss.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+})

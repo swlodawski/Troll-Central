fa6791c0c55bc4434e3b6674956cd459f2c1fdb1
sammy wlodawski <samwlodawski@gmail.com>
2024-08-29 15:53:49 -0400

    Finished the thoughts api routes.

--- routes/api/thoughtsRoute.js
+++ routes/api/thoughtsRoute.js
@@ -12,3 +12,8 @@ router.route('/').get(getThoughts).post(createThoughts);
 
 router.route('/:thoughtId').get(getSingleThoughts).delete(deleteThoughts).put(updateThought);
 
+router.route('./:thoughtId/reactions').post(addReaction);
+
+router.route('/thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;

2b0ad673fce1341a00c14f6684d8c53e114a7da6
sammy wlodawski <samwlodawski@gmail.com>
2024-08-30 07:29:33 -0400

    Additions.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const {User, Thought} = require('../models');
+const { create } = require('../models/Thoughts');
 
 module.exports = {
     async getUsers(req, res) {
@@ -13,9 +14,23 @@ module.exports = {
     async getSingleUser(req, res) {
         try
          {
-            const users = await User.findOne({_id: req.params.userId}).populate('thoughts');
+            const user = await User.findOne({_id: req.params.userId}).populate('thoughts');
 
-            
+            if(!user) {
+                return res.status(404).json({message: 'This user can not be found.'});
+            }
+            res.json(user);
+         } catch (err) {
+            res.status(500).json(err);
          }
+    },
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            
+        }
     }
 }

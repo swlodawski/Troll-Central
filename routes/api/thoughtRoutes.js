ddb2cba6cd97481f2a7ab63eca496b439def49cd
sammy wlodawski <samwlodawski@gmail.com>
2024-09-01 13:57:02 -0400

    Removed last of route test code and added description to package.json file.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,23 +1,3 @@
-// const router = require('express').Router();
-// const {
-//     getThoughts,
-//     getSingleThought,
-//     createThought,
-//     deleteThought,
-//     addReaction,
-//     removeReaction
-// } = require('../../controllers/thoughtController'); 
-
-// router.route('/').get(getThoughts).post(createThought);
-
-// router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
-
-// router.route('/:thoughtId/reactions').post(addReaction);
-
-// router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
-
-// module.exports = router;
-
 const router = require('express').Router();
 const {
   getThoughts,
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,27 +1,3 @@
-// const router = require('express').Router();
-
-// const {
-//     getUsers,
-//     getSingleUser,
-//     createUser,
-//     updateUser,
-//     deleteUser,
-//     addFriend,
-//     removeFriend
-// } = require('../../controllers/userController');
-
-// router.route('/').get(getUsers).post(createUser);
-
-// router.route('/:userId/:friendId').post(addFriend).delete(removeFriend);
-
-// router 
-// .route('/:userId')
-// .get(getSingleUser)
-// .put(updateUser)
-// .delete(deleteUser);
-
-// module.exports = router;
-
 const router = require('express').Router();
 const {
   getUsers,
